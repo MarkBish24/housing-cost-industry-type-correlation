f6e0fc0ded2c226c5872a39b0ee95a67e5ffff18
Mark Bishoff <markbish24@gmail.com>
2025-08-30 08:03:49 -0700

    something

--- frontend/src/components/ChartSelection.jsx
+++ frontend/src/components/ChartSelection.jsx
@@ -1,32 +1,31 @@
 import { BarChart2, LineChart, ScatterChart } from "lucide-react";
-import { FaChartBar, FaChartLine, FaChartPie } from "react-icons/fa";
 import { BiScatterChart } from "react-icons/bi";
 import { TbBox } from "react-icons/tb";
-import violinSvg from "../symbols/violin.svg";
+import ViolinIcon from "../symbols/violin.svg?react";
 
-const ViolinIcon = (props) => (
-  <img src={violinSvg} alt="Violin Chart" {...props} />
-);
+// const ViolinIcon = (props) => (
+//   <img src={violinSvg} alt="Violin Chart" {...props} />
+// );
 
 const IconList = [
   { name: "Bar Chart", icon: BarChart2 },
   { name: "Line Chart", icon: LineChart },
   { name: "Scatter Plot", icon: ScatterChart },
-  { name: "Box Plot", icon: TbBox },
   { name: "Bubble Chart", icon: BiScatterChart },
+  { name: "Box Plot", icon: TbBox },
   { name: "Violin Plot", icon: ViolinIcon },
 ];
 
 export default function ChartSelection() {
   return (
-    <div className="flex flex-wrap gap-4 p-4 justify-center">
+    <div className="flex flex-wrap gap-1 p-2 justify-center border rounded-xl shadow-sm">
       {IconList.map(({ name, icon: Icon }) => (
         <div
           key={name}
-          className="flex flex-col items-center justify-center p-3 border rounded-xl shadow-sm hover:bg-gray-100 transition"
+          className="flex flex-col items-center justify-center p-2 border rounded-xl shadow-sm hover:bg-gray-100 transition cursor-pointer"
+          title={name}
         >
-          <Icon className="w-8 h-8 mb-2" />
-          <span className="text-sm">{name}</span>
+          <Icon className="w-4 h-4" />
         </div>
       ))}
     </div>
